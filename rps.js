62bf0072ceed5ae5df327fcebe66b8db34f4fa6c
kowais915 <kowais915@gmail.com>
2021-07-18 13:39:45 +0500

    added a single round function

---
+++ rps.js
@@ -0,0 +1,71 @@
+// Rock Paper Scissor game against computer
+
+
+// Psudocode
+
+//this function returns items in the array randomly
+function compPlay(){
+    let arr = ['rock', 'paper', 'scissor'];
+    return arr[Math.floor(Math.random()*3)];
+}
+
+
+/*
+player computer
+rock   scissor (player wins)
+rock   paper   (computer wins)
+rock   rock    (ties)
+
+paper  scissor (computer wins)
+paper  paper   (ties)
+paper  rock    (player wins)
+
+scissor scissor (ties)
+scissor paper   (player wins)
+scissor rock    (computer wins)
+
+
+*/
+
+let comp = compPlay();
+
+
+function rounds(player, comp){
+
+
+
+
+
+    
+    if(player==comp){
+        return ("Game Tied!");
+    }
+    else if(player=="rock" && comp =="scissor"){
+        let str1 = "You won! Rock beats scissors";
+        return str1;
+    }
+    else if(player=="rock" && comp == "paper" ){
+        return ("You lost! Paper covers rocks.");
+    }
+    else if(player=="paper" && comp == "scissor"){
+        return ("You lost! Scissor cuts the paper.");
+    }
+    else if(player=="paper" && comp =="rock"){
+        return ("You won! Paper covers the rock.");
+
+    }
+
+    else if(player=="scissor" && comp == "paper"){
+        return ("You Won! Scissor cuts the paper.");
+    }
+    else if(player=="scissor" && comp=="rock"){
+        return ("You lost! Rock beats the scissor.");
+    }
+    else{
+        return ("invalid Input!");
+    }
+
+
+}
+
+console.log(rounds("rock", comp));
